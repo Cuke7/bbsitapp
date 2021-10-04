0b544e92cecc508df73ea45a69ad665f31f9d0ef
Cuke7 <cuke.dev@mail.com>
2021-10-04 19:15:31 +0200

    Added share (almost lol)

---
+++ pages/_bbsitID.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ this.bbsitID }}</h1>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const bbsitID = params.bbsitID; // When calling /abc the slug will be "abc"
+    return { bbsitID };
+  }
+};
+</script>
